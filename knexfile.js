88f3491f85026e04c47812d823b169c3ba58579b
Ken Kotch <hello@ken.uno>
2017-12-17 10:20:06 -0700

    update knexfile to remove test db

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,6 @@ module.exports = {
     connection: 'postgres://localhost/nice_paper_dev'
   },
 
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/nice_paper_test'
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
